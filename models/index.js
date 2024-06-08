bc468c491e5650fd591ef6470454f566687301a9
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-06-08 13:58:22 -0500

    feat: modified index.js model

--- models/index.js
+++ models/index.js
@@ -1,15 +1,22 @@
-// import models
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URI);
-const db = mongoose.connection;
-
-db.once('open', () => console.log(`Connected to MongoDB at ${db.host}:${db.port}`));
-db.on('error', (error) => console.log('Database error \n', error));
-
+// models
 const User = require('./user');
 
-module.exports = {
-    // models go
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// export models
+module.exports = { 
     User,
+
 }
